d6f6949ff7b7f62ae0892f911bcd171fad713611
GavinD36 <gavin.desris1@student.northampton.edu>
2026-02-01 19:19:16 +0000

    Rectangle Properties Gavin Desris

--- main.cpp
+++ main.cpp
@@ -4,6 +4,17 @@ using namespace std;
 int main() {
   const double length = 7.2;
   const double width = 1.3;
+  double area;
+  double perimeter;
 
+  cout << "Rectangle Properties" << endl;
+  cout << "Length = " << length << endl;
+  cout << "Width = " << width << endl;
+  
+  area = length*width;
+  cout << "Area = " << area << endl;
 
+  perimeter = (length*2)+(width*2);
+  cout << "Perimeter = " << perimeter << endl;
+  
 }
